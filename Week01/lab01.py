67436f36f125e55f926a648094cea50178e0b0a3
comp2152 <clein.ecoy@gmail.com>
2026-01-16 16:06:18 -0500

    adding q1, q2, q3

---
+++ Week01/lab01.py
@@ -0,0 +1,5 @@
+# Sample Coding Questions 01 week 01 
+# Clein Ecoy
+
+# Question 2 
+my_array = [1, 2, 3, 4]
---
+++ Week01/q3.py
@@ -0,0 +1,10 @@
+# question 3: order of Operations 
+a = 1
+b = 2
+c = 3
+d = 4 
+e = 5
+
+
+# Fully-Bracketed version of: e = a - b ** c // d + a % c
+e = (  - ((b ** c) // d)) + ( a % c )
