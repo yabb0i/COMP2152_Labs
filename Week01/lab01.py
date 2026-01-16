406cb8b07dde11cded9dd0d24bf88a242adcb9cd
comp2152 <clein.ecoy@gmail.com>
2026-01-16 16:16:17 -0500

    add lab 01 with answers

--- Week01/lab01.py
+++ Week01/lab01.py
@@ -3,3 +3,24 @@
 
 # Question 2 
 my_array = [1, 2, 3, 4]
+
+# question 3: order of Operations 
+a = 1
+b = 2
+c = 3
+d = 4 
+e = 5
+
+
+# Fully-Bracketed version of: e = a - b ** c // d + a % c
+e = (  - ((b ** c) // d)) + ( a % c )
+
+# question 4: Formatting
+temperature = 32.6
+print("The temperature today is: {:#f} degerees Celsius".format(temperature))
+
+
+# question 5 : Userinput
+userAge = int(input("enter your age"))
+userAge = userAge + 22
+print("Now showing the shop items filtered by age: ", userAge)
---
+++ Week01/userInput.py
@@ -0,0 +1,4 @@
+# question 5 : Userinput
+userAge = int(input("enter your age"))
+userAge = userAge + 22
+print("Now showing the shop items filtered by age: ", userAge)
